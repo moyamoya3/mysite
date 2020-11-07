923f50fa0895bec38eefd5b593550ad29a093c13
moyamoya3 <69374688+moyamoya3@users.noreply.github.com>
2020-11-07 12:02:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import json
-from Linebot import LineBotApi
-from Linebot.models import TextSendMessage
+# import json
+# from Linebot import LineBotApi
+# from Linebot.models import TextSendMessage
 
-file = open('info.json', 'r')
-info = json.load(file)
+# file = open('info.json', 'r')
+# info = json.load(file)
 
-CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-def main():
-	USER_ID = info['USER_ID']
-	messages = TextSendMessage(text='hallo')
-	line_bot_api.push_message(USER_ID, messages=messages)
+# CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
+# line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+# def main():
+# 	USER_ID = info['USER_ID']
+# 	messages = TextSendMessage(text='hallo')
+# 	line_bot_api.push_message(USER_ID, messages=messages)
 
-if __name__ == "__main__":
-	main()
+# if __name__ == "__main__":
+# 	main()
 
 
-    
+with open('abc.txt', 'w') as f:
+	f.write('aaa'
