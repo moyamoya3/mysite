794e4c45aaa20164daeab1404217cc47f6d4b5fb
moyamoya3 <masaaki.oyaizu@gmail.com>
2020-11-07 17:06:17 +0900

    Updated!

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import json
-from Linebot import LineBotApi
-from Linebot.models import TextSendMessage
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 
 file = open('info.json', 'r')
 info = json.load(file)
 
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+text = ['abcdefg']
+text = 2*text
+text = '****'.join(text)
+
 def main():
 	USER_ID = info['USER_ID']
-	messages = TextSendMessage(text='hallo')
+	messages = TextSendMessage(text=text)
 	line_bot_api.push_message(USER_ID, messages=messages)
 
 if __name__ == "__main__":
