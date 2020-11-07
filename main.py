00043021fb2b278b1d16cd4f5741af88dc3b31b3
moyamoya3 <masaaki.oyaizu@gmail.com>
2020-11-07 17:34:05 +0900

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ info = json.load(file)
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-text = ['abcdefg']
+text = ['1733']
 text = 2*text
 text = '****'.join(text)
 
@@ -21,6 +21,6 @@ if __name__ == "__main__":
 	main()
 
 with open('abc.txt', 'w') as f:
-	f.write('aaa')
+	f.write('1733')
 
     
